e90165d6624ce06b138b7a5189ad38a9bddf41d0
Josue Lopes <josue.s.lopes.512@gmail.com>
2021-05-21 01:51:52 -0300

    debug-21/05

---
+++ grammar.py
@@ -0,0 +1,57 @@
+import re
+
+class Grammar:
+    def __init__(self, rules):
+        rules = tuple(rules)
+        self.rules = tuple(self._parse(rule) for rule in rules)
+
+    def _parse(self, rule):
+        return tuple(rule.replace(' ', '').split('->'))
+        
+    def __getitem__(self, nonterminal):
+        yield from [rule for rule in self.rules if rule[0] == nonterminal]
+        
+    @staticmethod 
+    def is_nonterminal(symbol):
+        return symbol.isalpha() and symbol.isupper()
+
+    @property
+    def nonterminals(self):
+        return set([nt for nt, _ in self.rules])
+        
+    @property
+    def terminals(self):       
+        a = [
+            expression
+            for _, expression in self.rules
+            if not self.is_nonterminal(expression)
+        ]
+        
+        nt = sorted([nt for nt, _ in self.rules], key=len, reverse=True)
+
+        for i in nt:
+            for j in range(0, len(a)):
+                if re.search(i, a[j]):
+                    a[j] = a[j].replace(i, '')
+                a[j] = " ".join(a[j].split())
+        
+        tmp = []  
+        for i in a:
+            if i != '' and i not in tmp:
+                tmp.append(i)
+        a = tmp
+
+        tmp = []
+        for i in a:
+            if len(i) > 1 and not i.isalpha():
+                tmp2 = []
+                tmp2.append(list(i))
+                tmp2 = [i for j in tmp2 for i in j]
+                for j in tmp2:
+                    tmp.append(j)
+            else:
+                tmp.append(i)
+        a = tmp
+        tmp = []
+        # (sorted([i for i in a], key=len, reverse=True))        
+        return set(a) 
